5873e782e9c5b439548cf8e1c8ddbd89fa744450
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-05-25 20:38:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TBEXMZF6J7VWNXBFQE2XXOOWFGBV3N7CQH5JI6A")
 console.log("Hello Symbol")
+console.log(`Your Address : ${address.plain()}`)
