18bb77181d7d6a5637f5be33578c2f647532f666
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-05-25 20:52:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TBEXMZF6J7VWNXBFQE2XXOOWFGBV3N7CQH5JI6A")
+
 const dom_addr = document.getElementById('wallet-addr')
 dom_addr.innerText = address.pretty()
 
-const address = symbol.Address.createFromRawAddress("TBEXMZF6J7VWNXBFQE2XXOOWFGBV3N7CQH5JI6A")
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
