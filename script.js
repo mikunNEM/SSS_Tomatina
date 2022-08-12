47033de440c1233c214ae71db18707b49d1901aa
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-13 00:42:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -58,11 +58,11 @@ accountHttp.getAccountInfo(address)
         my_audio.currentTime = 0;  //再生開始位置を先頭に戻す
         my_audio.play();  //サウンドを再生
       
-        setTimeout(function(){             //1秒待つ
-             console.log("Wait 1 seconds");
-        },1000);
+       // setTimeout(function(){             //1秒待つ
+       //      console.log("Wait 1 seconds");
+       // },1000);
       
-        location.reload(true); // ページをリロードする
+        //location.reload(true); // ページをリロードする
     });
 
     //未承認トランザクションの検知
