6889065ab61f163c8f5bb2e48d88001bffffffe7
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-13 00:24:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,11 @@ accountHttp.getAccountInfo(address)
         var my_audio = new Audio("https://github.com/symbol/desktop-wallet/raw/dev/src/views/resources/audio/ding2.ogg");
         my_audio.currentTime = 0;  //再生開始位置を先頭に戻す
         my_audio.play();  //サウンドを再生
+      
+        setTimeout(function(){
+             console.log("Wait 1 seconds");
+        },1000);
+      
         location.reload(true); // ページをリロードする
     });
 
