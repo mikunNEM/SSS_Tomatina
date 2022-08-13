57513af9a72a04072ad8a64f92839407406b7a63
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-13 12:31:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -131,11 +131,10 @@ transactionHttp
       console.log(tx.signer.address.address);
      
      // if(tx.signer.address.address === address) {
-         dom_amount.innerText = `三╰( `•ω•)╮-=ﾆ=ﾟ｡🍅 : ${tx.mosaics[0].amount.lower}`     // 　数量     
+         dom_amount.innerText = `🍅 : ${tx.mosaics[0].amount.lower}`     // 　数量     
       //}else { 
       //   dom_amount.innerText = `＝🍅)`3゜)∵ : ${tx.mosaics[0].amount.lower}`     // 　数量 
      // }
-      
       dom_message.innerText = `Message : ${tx.message.payload}`     // 　メッセージ 
       
     }
