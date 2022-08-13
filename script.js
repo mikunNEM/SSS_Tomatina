0b00d361b2f8a7183e025d7404f8a9cb6b5476ee
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-13 16:34:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ setTimeout(() => {
   
 const address = symbol.Address.createFromRawAddress(window.SSS.activeAddress)
   
-const address_a = JSON.stringify(address);                //[object object] をstring化する （アドレスチェック用: address_a）
+//const address_a = JSON.stringify(address);                //[object object] をstring化する （アドレスチェック用: address_a）
 
 const dom_addr = document.getElementById('wallet-addr')
 dom_addr.innerText = address.pretty()                                       // address.pretty() アドレスがハイフンで区切られた文字列で表示され見やすくなる
@@ -129,9 +129,9 @@ transactionHttp
       dom_recipient_address.innerText = `To   : ${tx.recipientAddress.address}`//  文字列の結合　宛先
       
       
-      console.log(`address_a = ${address_a}`); ////////////////////
+      console.log(`address.address = ${address.address}`); ////////////////////
      
-      if(tx.signer.address.address === address_a.address) {  // 送信アドレスとウォレットのアドレスが同じかどうかで表示を変える
+      if(tx.signer.address.address === address.address) {  // 送信アドレスとウォレットのアドレスが同じかどうかで表示を変える
          dom_amount.innerText = `🍅➡️ : ${tx.mosaics[0].amount.lower}`     // 　数量
          console.log("1234");
       }else { 
