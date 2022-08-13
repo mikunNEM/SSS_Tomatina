3889d5b74999ccb33b66dfb4a00dd22ec514b138
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-13 12:48:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ transactionHttp
       if(tx.signer.address.address === address) {
          dom_amount.innerText = `🍅➡️ : ${tx.mosaics[0].amount.lower}`     // 　数量     
       }else { 
-         dom_amount.innerText = `➡️🍅 : ${tx.mosaics[0].amount.lower}`     // 　数量 
+         dom_amount.innerText = `⬅️🍅 : ${tx.mosaics[0].amount.lower}`     // 　数量 
       }
       dom_message.innerText = `Message : ${tx.message.payload}`     // 　メッセージ 
       
